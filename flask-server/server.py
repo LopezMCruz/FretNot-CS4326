b0719f4768e20abc14a6836699db5701476cd3c4
djfox90 <djfox232@gmail.com>
2023-03-14 16:16:14 -0500

    added html and css to folder

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,13 +1,18 @@
 from durable.lang import *
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response,render_template
+
 import sqlite3
 import json
 
 app = Flask(__name__)
 
+@app.route("/")
 @app.route("/members")
 def members():
-    return{"members": ["Member1","Member2","Member3"]}
+    test = "test template"
+    return render_template("Fret.html",member=members,testing = test )#test is just for testing purpose
+    #return {"members": ["Member1","Member2","Member3"]}
+    
 
 if __name__=="__main__":
     app.run(debug=True)
