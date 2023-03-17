8fada9d34529bb08bd60367c0e0fc0ed991d8955
Cruz Lopez <cml231@txstate.edu>
2023-03-17 02:42:23 -0500

    added Chord Dictionary and methods to print string to page

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,18 +1,23 @@
-from durable.lang import *
-from flask import Flask, request, make_response,render_template
+from flask import Flask, request, make_response, jsonify
+from catalog import chords
 
-import sqlite3
-import json
 
 app = Flask(__name__)
 
+# Looks up chord based on input
+def show_chord(chords, value):
+    chord = chords[value]
+    if value in chords:
+        return("chord: " + chord["chord"])
+    else:
+        return("not found")
+
+# passes chord to the web page
 @app.route("/")
-@app.route("/members")
-def members():
-    test = "test template"
-    return render_template("Fret.html",member=members,testing = test )#test is just for testing purpose
-    #return {"members": ["Member1","Member2","Member3"]}
-    
+def get_chord():
+    value = "2,x,1,3,x,x,A#,G,C#"
+    new_chord=show_chord(chords, value)
+    return jsonify(new_chord)
 
 if __name__=="__main__":
     app.run(debug=True)
