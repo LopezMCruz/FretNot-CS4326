66883c631ba8f8d813dc3ed93bbc7b96f2798814
djfox90 <djfox232@gmail.com>
2023-03-18 16:05:23 -0500

    render_template for feedback form

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,4 +1,4 @@
-from durable.lang import *
+#from durable.lang import *
 from flask import Flask, request, make_response,render_template
 
 import sqlite3
@@ -12,7 +12,9 @@ def members():
     test = "test template"
     return render_template("Fret.html",member=members,testing = test )#test is just for testing purpose
     #return {"members": ["Member1","Member2","Member3"]}
-    
+@app.route("/feedback_form")
+def feedback():
+    return render_template("feedback_form.html")  
 
 if __name__=="__main__":
     app.run(debug=True)
