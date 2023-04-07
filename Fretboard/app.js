9759f1f8528a286c154f465cf465fe61e2ece69f
Matteo Coffman <matteo.coffman@gmail.com>
2023-04-07 16:25:25 -0500

    Allowed for selection of frets

--- Fretboard/app.js
+++ Fretboard/app.js
@@ -18,26 +18,39 @@ const app = {
 
     setup() {
         fretboard.innerHTML = '';
-        //root.style.setProperty('--number-of-strings', numberOfStrings);
-        // Add strings to fretboard
+    
         for (let i = 0; i < 6; i++) {
             let string = tools.createElement('div');
             string.classList.add('string');
             fretboard.appendChild(string);
-         
-            // Create frets
+    
+            let selectedNoteFret;
+    
             for (let fret = 0; fret <= 12; fret++) {
                 let noteFret = tools.createElement('div');
                 noteFret.classList.add('note-fret');
                 string.appendChild(noteFret);
-                
+    
                 let noteName = this.createNoteNames(fret + guitarTuning[i]);
                 noteFret.setAttribute('data-note', noteName);
-
+    
+                noteFret.addEventListener('click', function() {
+                    if (noteFret.classList.contains('selected')) {
+                        noteFret.classList.remove('selected');
+                        selectedNoteFret = null;
+                    } else {
+                        if (selectedNoteFret) {
+                            selectedNoteFret.classList.remove('selected');
+                        }
+                        noteFret.classList.add('selected');
+                        selectedNoteFret = noteFret;
+                    }
+                });
+    
                 if (i === 0 && singleFretMarkPositions.indexOf(fret) !== -1) {
                     noteFret.classList.add('single-fretmark');
                 }
-
+    
                 if (i === 0 && doubleFretMarkPositions.indexOf(fret) !== -1) {
                     let doubleFretMark = tools.createElement('div');
                     doubleFretMark.classList.add('double-fretmark');
@@ -46,6 +59,8 @@ const app = {
             }
         }
     },
+    
+    
 
     createNoteNames(noteIndex){
         noteIndex = noteIndex %  12; 
