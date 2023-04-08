ed5ea32286c7eb4e96099cb9a5cdd1bf267969cb
Cruz Lopez <cml231@txstate.edu>
2023-04-07 23:59:22 -0500

    updated chord analyzer logic

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,65 +1,57 @@
-from flask import Flask, request, make_response, jsonify
+#from durable.lang import *
+from flask import Flask, request, make_response,render_template, jsonify, redirect, url_for
 from catalog import chords
-
 
 app = Flask(__name__)
 
 # Looks up chord based on input
+# chords argument is imported from catalog.py
+# "from catalog import chords"
 def show_chord(chords, value):
     chord = chords[value]
     if value in chords:
         return("chord: " + chord["chord"])
     else:
         return("not found")
-    
-# passes chord to the web page
-@app.route("/")
-def get_chord():
-    value = "2,x,1,3,x,x,A#,G,C#"
-    new_chord=show_chord(chords, value)
-    return jsonify(new_chord)
 
-
-notes = []
-
-# Initialize empty array
+# Initialize empty array, an index for every string
 my_array = [None, None, None, None, None, None]
-my_array2 = [None, None, None, None, None, None]
-
-@app.route('/')
-def index():
-  return render_template('index.html')
 
 @app.route('/update-array', methods=['POST'])
 def update_array():
-  value = request.json['value']
-  index = int(request.json['index'])
-  if value == my_array[index]:
-    my_array[index] = None
-  else:
-    my_array[index] = value
-  for i in range(len(my_array)):
-    if my_array is None:
-        my_array2[index] = '0'
+    value = request.json['value'] #gets the value from whatever note is active
+    index = int(request.json['index']) #the index represents a string on the fretboard
+    if value == my_array[index]: #this if statement removes the note if it is selected a second time
+        my_array[index] = None
     else:
-        my_array2[index] = 'x'
-  query = arr2string(my_array, my_array2)       
-  print(query)
-  return ''
+        my_array[index] = value # updates the array with the note ex: [None, E, C, None, A, None]
+    # the array is converted to a string with the 'None' values stripped out
+    query = ",".join(str(x) for x in my_array if x is not None) #ex: ex: [None, E, C, None, A, None] becomes E,C,E
+    new_chord = show_chord(chords, query) #queries catalog.py for matching chords
+    if new_chord == "":
+        return "No Chord Found"
+    else:
+        return jsonify({'chord': new_chord})
+    #return jsonify({'chord': new_chord}) #returns a json 
 
-def arr2string(array1, array2):
-    combined_string = ''
-    for i in range(len(array1)):
-        combined_string += array1[i] + ' ' + array2[i] + ', '
-    combined_string = combined_string[:-2]
-    return combined_string
+#this is the middle man tha lets the program use the array from /update-array
+@app.route('/update-and-get-chord', methods=['POST'])
+def update_and_get_chord():
+    update_array()
+    return redirect(url_for('get_chord'))
 
-#@app.route("/")
-#@app.route("/members")
-#def members():
-#    test = "test template"
-#    return render_template("Fret.html",member=members,testing = test )#test is just for testing purpose
-    #return {"members": ["Member1","Member2","Member3"]}
+# dulplicate code is because the chord name needs to be calculated 
+# everytime the chord is updated. This will need to be rewritten
+# when I have more time
+@app.route('/get_chord')
+def get_chord():
+    query = ",".join(str(x) for x in my_array if x is not None) 
+    new_chord = show_chord(chords, query)
+    if new_chord == "":
+        return "No Chord Found"
+    else:
+        return jsonify({'chord': new_chord})
+
 
 @app.route("/feedback_form")
 def feedback():
@@ -68,10 +60,9 @@ def feedback():
 def discovery_page():
     return render_template("discovery.html")
 
-@app.route("/fretboard")
+@app.route("/fretboard", methods=['GET','POST'])
 def fretboard():
     return render_template("prototype_fretboard.html")
 
 if __name__=="__main__":
     app.run(debug=True)
-
--- flask-server/static/fretboard.js
+++ flask-server/static/fretboard.js
@@ -15,92 +15,21 @@ cells.forEach(cell => {
     // Update array with value of clicked cell
     const value = e.target.dataset.value;
     const index = e.target.dataset.index;
-    fetch('/update-array', {
+    fetch('/update-array', { // calls the /update-array route from server.py
       method: 'POST',
       body: JSON.stringify({ index: index, value: value }),
       headers: { 'Content-Type': 'application/json' }
+    }).then(() => {
+      fetch('/get_chord') // calls the /get_chord route from server.py
+        .then(response => response.json())
+        .then(data => {
+          if (data === "No Chord Found") { // this is supposed update on the webpage
+                                            // when no chord is found but it's not working yet
+              document.getElementById('chord-name').innerHTML = data;
+          } else { //else statment pushes the chord json from get_chord to the html block
+              document.getElementById('chord-name').innerHTML = data.chord;
+          }
+        });
     });
   });
 });
-
-/** 
-const rows = document.querySelectorAll('.row');
-
-  rows.forEach(row => {
-    const cells = row.querySelectorAll('.cell');
-    cells.forEach(cell => {
-      cell.addEventListener('click', (e) => {
-        const clickedCell = e.target;
-        const activeCell = row.querySelector('.cell.active');
-        if (activeCell) {
-          activeCell.classList.remove('active');
-        }
-        clickedCell.classList.add('active');
-      });
-    });
-  });
-*/
-
-/**
- 
- 
-const cells = document.querySelectorAll('.cell');
-
-let activeCell;
-
-cells.forEach(cell => {
-    cell.addEventListener('click', e => {
-      // Remove the "active" class from the previously active cell
-      if (activeCell) {
-        activeCell.classList.remove('active');
-      }
-  
-      // Add the "active" class to the clicked cell
-      cell.classList.add('active');
-      activeCell = cell;
-    });
-  });
-  **/
-/** 
-function addClickEvent(elementID){
-    const element = document.getElementById(elementID);
-    element.addEventListener('click', (e) => {
-        fretClick(element);
-    });
-}
-addClickEvent('E1');
-addClickEvent('F1');
-addClickEvent('Gb1');
-addClickEvent('G1');
-addClickEvent('Ab1');
-addClickEvent('A1');
-addClickEvent('Bb1');
-addClickEvent('B1');
-addClickEvent('C1');
-addClickEvent('Db1');
-addClickEvent('D1');
-addClickEvent('Eb1');
-addClickEvent('E2');
-addClickEvent('F2');
-addClickEvent('Gb2');
-addClickEvent('G2');
-
-function e1StringClick(key){
-    let E=document.getElementById('E1');
-    let F=document.getElementById('F1');
-    let Gb1=document.getElementById('Gb1');
-    let G=document.getElementById('G1');
-    let Ab=document.getElementById('Ab1');
-    let A=document.getElementById('A1');
-    let Bb=document.getElementById('Bb1');
-    let B=document.getElementById('B1');
-    let C=document.getElementById('C1');
-    let Db=document.getElementById('Db1');
-    let D=document.getElementById('D1');
-    let Eb=document.getElementById('Eb1');
-    let E2=document.getElementById('E2');
-    let F2=document.getElementById('F2');
-    let Gb2=document.getElementById('Gb2');
-    let G2=document.getElementById('G2');
-}
-**/
